e86bba8dee8b21b7bb0f5b9d33cdea7b6f30022f
Fabi121 <Roxy-75@t-online.de>
2021-09-08 12:38:16 +0200

    Abstrakte Klassen - Update

--- Node.java
+++ Node.java
@@ -1,6 +1,8 @@
 import java.util.LinkedList;
 import java.util.List;
 
+
+//Abstrakte Klassen für alle folgenden Node-Typen
 public abstract class Node {
     Token start, end;
 
@@ -10,9 +12,112 @@ public abstract class Node {
     }
     public abstract String toString(String indent);
 
-    //public void semantischeAnalyse(SymbolTabelle tabelle, List<CompilerError> errors) {}
+    public void semantischeAnalyse(SymbolTabelle tabelle, List<CompilerError> errors) {}
 
     public void run() {}
+}
+
+abstract class ExprNode extends Node {
+    Type type;
+
+    public ExprNode(Token start, Token end) {
+        super(start, end);
+    }
+    public abstract Type semantischeAnalyseExpr(SymbolTabelle tabelle, List<CompilerError> errors);
+    public abstract Value runExpr();
+    public void run() { runExpr(); }
+}
+
+abstract class NumberNode extends ExprNode {
+    Token content;
+    public NumberNode(Token content) {
+        super(content,content);
+        this.content = content;
+    }
+}
+
+abstract class StmntNode extends Node {
+    public StmntNode(Token start, Token end) {
+        super(start,end);
+    }
+}
+
+
+
+
+
+
+class EmptyStmntNode extends StmntNode {
+    public EmptyStmntNode(Token start) {
+        super(start,start);
+    }
+    public String toString(String indent) {
+        return indent+"EmptyStmnt";
+    }
+
+    public void semantischeAnalyse(SymbolTabelle tabelle, List<CompilerError> errors) { }
+}
+
+
+
+
+class IfNode extends StmntNode {
+    ExprNode expr;
+    StmntNode stmnt;
+    StmntNode elseStmnt;
+
+    public IfNode(Token start, ExprNode expr, StmntNode stmnt, StmntNode elseStmnt) {
+        super(start, elseStmnt!=null ? elseStmnt.end : stmnt.end);
+        this.expr = expr;
+        this.stmnt = stmnt;
+        this.elseStmnt = elseStmnt;
+    }
+    public String toString(String indent) {
+        return indent+"IfStatement\n"
+                +expr.toString(indent+"\t")+"\n"
+                +stmnt.toString(indent+"\t")
+                + (elseStmnt!=null ? "\n"+elseStmnt.toString(indent+"\t") : "");
+    }
+    public void semantischeAnalyse(SymbolTabelle tabelle, List<CompilerError> errors) {
+        if (expr.semantischeAnalyseExpr(tabelle,errors) != Type.booleanType)
+        {
+            errors.add(new SemanticError(start,end,"if-expression must be boolean"));
+        }
+        stmnt.semantischeAnalyse(tabelle, errors);
+        if (elseStmnt!=null) elseStmnt.semantischeAnalyse(tabelle, errors);
+    }
+    public void run() {
+        Value bed = expr.runExpr();
+        if (bed.b) stmnt.run();
+        else if (elseStmnt!=null) elseStmnt.run();
+    }
+}
+
+class WhileNode extends StmntNode {
+    ExprNode expr;
+    StmntNode stmnt;
+
+    public WhileNode(Token start, ExprNode expr, StmntNode stmnt) {
+        super(start, stmnt.end);
+        this.expr = expr;
+        this.stmnt = stmnt;
+    }
+    public String toString(String indent) {
+        return indent+"WhileStatement\n"
+                +expr.toString(indent+"\t")+"\n"
+                +stmnt.toString(indent+"\t");
+    }
+    public void semantischeAnalyse(SymbolTabelle tabelle, List<CompilerError> errors) {
+        if (expr.semantischeAnalyseExpr(tabelle, errors) != Type.booleanType)
+        {
+            errors.add(new SemanticError(start,end,"while-condition must be boolean"));
+        }
+        stmnt.semantischeAnalyse(tabelle, errors);
+    }
+    public void run() {
+        Value bed = expr.runExpr();
+        while (bed.b) stmnt.run();
+    }
 }
 
 
