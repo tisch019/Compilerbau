386c5ad3be294cff8ee3679ec7bd8e780617d474
tisch019 <timo.schotka@googlemail.com>
2021-09-09 23:55:04 +0200

    Identifier im Filter Interpretieren

--- Filter.java
+++ Filter.java
@@ -1,10 +1,25 @@
 import java.io.IOException;
 import java.util.List;
 import java.util.Set;
+import java.util.LinkedList;
 
 class Filter{
     int current;
     List<InterpreterError> errors;
+    LinkedList<Token> tokenlist = new LinkedList<>();
+    Lexer lexer;
+    int index;
+    int line = 1;
+    int column = 1;
+
+    public Filter(Lexer lexer, List errors) {
+        this.lexer = lexer;
+        this.errors = errors;
+    }
+
+    void fillBuffer(int n) throws IOException {
+        for (int i=0; i<n;i++) tokenlist.add(getNextFilteredToken());
+    }
 
     public void matchToken(Token.Type type, Set<Token.Type> sync) throws IOException, ParserError{
         if (getToken().kind == type) matchToken();
@@ -17,9 +32,47 @@ class Filter{
         }
     }
 
+    Token getNextFilteredToken() throws IOException {
+        Token currentToken;
+        do {
+            currentToken = lexer.getNextToken();
+            currentToken.line = line;
+            currentToken.column = column;
+            if (currentToken.content.equals("\n") || currentToken.content.equals("\r")) {
+                line++;
+                column = 1;
+            }
+            else column += currentToken.content.length();
+            if (currentToken.kind == Token.Type.ERROR) errors.add(new LexerError(currentToken, "Unknown token " + currentToken.content));
+        } while (currentToken.kind == Token.Type.WS || currentToken.kind == Token.Type.ERROR);
+        if (currentToken.kind == Token.Type.IDENTIFIER) {
+            switch (currentToken.content) {
+                case "String": currentToken.kind = Token.Type.KEYSTRING; break;
+                case "double": currentToken.kind = Token.Type.KEYDOUBLE; break;
+                case "int": currentToken.kind = Token.Type.KEYINT; break;
+                case "boolean": currentToken.kind = Token.Type.KEYBOOL; break;
+                case "false":
+                case "true": currentToken.kind = Token.Type.BOOL; break;
+                case "if": currentToken.kind = Token.Type.IFSTART; break;
+                case "else": currentToken.kind = Token.Type.ELSE; break;
+                case "while": currentToken.kind = Token.Type.WHILE; break;
+                case "Range": currentToken.kind = Token.Type.KEYRANGE; break;
+                case "State": currentToken.kind = Token.Type.KEYSTATE; break;
+                case "Transition": currentToken.kind = Token.Type.KEYTRANSITION; break;
+                case "FA": currentToken.kind = Token.Type.KEYFA; break;
+                case "RA": currentToken.kind = Token.Type.KEYRA; break;
+                case "Set": currentToken.kind = Token.Type.KEYSET; break;
+                case "Map": currentToken.kind = Token.Type.KEYMAP; break;
+                case "print": currentToken.kind = Token.Type.PRINT; break;
+            }
+        }
+        currentToken.index = index++;
+        return currentToken;
+    }
+
     public Token getToken(int i) throws IOException{
-        //TODO
-        return null;
+        fillBuffer(current+i+1 - tokenlist.size());
+        return tokenlist.get(current+i);
     }
 
     public Token getToken() throws IOException {
