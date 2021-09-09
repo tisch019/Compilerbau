94a627b02fb4a4dca3c03cb2ed5ed5366885cd04
DerMilchmann <jakob.rhb@googlemail.com>
2021-09-09 18:12:08 +0200

    Error Sync Matching

--- Filter.java
+++ Filter.java
@@ -1,18 +1,31 @@
 import java.io.IOException;
+import java.util.List;
 import java.util.Set;
 
 class Filter{
-    public Token getToken() throws IOException{
-        return null;
-    }
+    int current;
+    List<InterpreterError> errors;
 
-    public void matchToken(Token.Type identifier, Set<Token.Type> sync) throws IOException, ParserError{
-    }
-
-    public void matchToken() throws IOException{
+    public void matchToken(Token.Type type, Set<Token.Type> sync) throws IOException, ParserError{
+        if (getToken().kind == type) matchToken();
+        else {
+            Token currentToken = getToken();
+            ParserError error = new ParserError(currentToken, type+" expected instead of "+currentToken.kind);
+            errors.add(error);
+            while (!sync.contains(getToken().kind)) matchToken();
+            throw error;
+        }
     }
 
     public Token getToken(int i) throws IOException{
+        //TODO
         return null;
+    }
+
+    public Token getToken() throws IOException {
+        return getToken(0);
+    }
+    public void matchToken() {
+        current++;
     }
 }
