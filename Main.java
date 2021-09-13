a9e00355b7841c04f4b2ed5ce9e8ef9bf4efca01
DerMilchmann <jakob.rhb@googlemail.com>
2021-09-13 14:53:21 +0200

    Removed Imports

--- Main.java
+++ Main.java
@@ -1,8 +1,6 @@
 import java.io.IOException;
 import java.util.*;
 
-import TestComp.CUNode;
-
 public class Main{
     public static void main(String[] args) {
         List<InterpreterError> errors = new LinkedList<>();
